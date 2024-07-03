0179ad1cd6bc0384842b5c25318b9e4e09da645e
adrian-casabal-raphael <casabalraphael02@gmail.com>
2024-07-03 15:52:53 -0700

    added a way to insert characters, save to disk, delete characters, and created a functional enter key. Created a 'save as' feature.

--- main.c
+++ main.c
@@ -64,7 +64,7 @@ struct editorConfig E;
 /*** prototypes ***/
 void editorSetStatusMessage(const char *fmt, ...);
 void editorRefreshScreen();
-char *editorPrompt(char *prompt);
+char *editorPrompt(char *prompt, void (*callback)(char *, int));
 
 /*** terminal ***/
 void die(const char *s) {
@@ -190,6 +190,17 @@ int editorRowCxToRx(erow *row, int cx) {
         ++rx;
     }
     return rx;
+}
+
+int editorRowRxToCx(erow *row, int rx) {
+    int cur_rx = 0;
+    int cx;
+    for (cx = 0; cx < row->size; ++cx) {
+        if (row->chars[cx] == '\t') cur_rx += (KILO_TAB_STOP - 1) - (cur_rx % KILO_TAB_STOP);
+        ++cur_rx;
+        if (cur_rx > rx) return cx;
+    }
+    return cx;
 }
 
 void editorUpdateRow(erow *row) {
@@ -358,7 +369,7 @@ void editorOpen(char *filename) {
 
 void editorSave() {
     if (E.filename == NULL) {
-        E.filename = editorPrompt("Save as: %s (ESC to cancel)");
+        E.filename = editorPrompt("Save as: %s (ESC to cancel)", NULL);
         if (E.filename == NULL) {
             editorSetStatusMessage("Save aborted");
             return;
@@ -385,6 +396,25 @@ void editorSave() {
     editorSetStatusMessage("Can't save! I/O error: %s", strerror(errno));
 }
 
+/*** find ***/
+void editorFind() {
+    char *query = editorPrompt("Search: %s (ESC to cancel)", NULL);
+    if (query == NULL) return;
+
+    int i;
+    for (i = 0; i < E.numrows; ++i) {
+        erow *row = &E.row[i];
+        char *match = strstr(row->render, query);
+        if (match) {
+            E.cy = i;
+            E.cx = editorRowRxToCx(row, match - row->render);
+            E.rowoff = E.numrows;
+            break;
+        }
+    }
+    free(query);
+}
+
 /*** append buffer ***/
 struct abuf {
     char *b;
@@ -405,7 +435,7 @@ void abFree(struct abuf *ab) {
 }
 
 /*** input ***/
-char *editorPrompt(char *prompt) {
+char *editorPrompt(char *prompt, void (*callback)(char *, int)) {
     size_t bufsize = 128;
     char *buf = malloc(bufsize);
 
@@ -421,6 +451,7 @@ char *editorPrompt(char *prompt) {
         }
         else if (c == '\x1b') {
             editorSetStatusMessage("");
+            if (callback) callback(buf, c);
             free(buf);
             return NULL;
         }
@@ -438,6 +469,7 @@ char *editorPrompt(char *prompt) {
             buf[++buflen] = c;
             buf[buflen] = '\0';
         }
+        if (callback) callback(buf, c);
     }
 }
 void editorMoveCursor(int key) {
@@ -510,6 +542,11 @@ void editorProcessKeypress() {
         case END_KEY:
             if (E.cy < E.numrows) E.cx = E.row[E.cy].size;
             break;
+
+        case CTRL_KEY('f'):
+            editorFind();
+            break;
+
         case BACKSPACE:
         case CTRL_KEY('h'):
         case DEL_KEY:
@@ -695,7 +732,7 @@ int main(int argc, char *argv[]) {
         editorOpen(argv[1]);
     }
 
-   editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
+   editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
 
     while (1) {
         editorRefreshScreen();
